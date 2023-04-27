2b50280b96583a8984e98e5ef416dcd0ec4bcbc1
Usuario <Usuario@CPD02-M14>
2023-04-27 13:42:03 -0300

    Adicionar arquivos de projeto.

---
+++ AppBancoDigital/AppBancoDigital/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppBancoDigital.Services;
+using AppBancoDigital.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
