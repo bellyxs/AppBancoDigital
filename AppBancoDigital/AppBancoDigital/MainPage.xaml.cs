7500f58c761740e63e928e174d1b0e155b7c67b4
bellyxs <bellarodgs@gmail.com>
2023-05-15 09:39:25 -0300

    deixando os botoes transparentes

--- AppBancoDigital/AppBancoDigital/MainPage.xaml.cs
+++ AppBancoDigital/AppBancoDigital/MainPage.xaml.cs
@@ -15,6 +15,8 @@ namespace AppBancoDigital
         public MainPage()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
         }
 
         private void Button_Clicked(object sender, EventArgs e)
