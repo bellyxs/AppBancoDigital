3a51e679b4db0e15b5f27839aabf325f6f7f8506
bellyxs <bellarodgs@gmail.com>
2023-08-24 15:06:57 -0300

    tentando colocar uma combobox na opcao de recarga

---
+++ AppBancoDigital/AppBancoDigital/Model/Operadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Operadora
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public override string ToString()
+        {
+            return this.Id + " " + this.Name;
+        }
+    }
+}
